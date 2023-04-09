9a6b033bb1ce26224ae38ce0f770656deda4acc0
adam <115082272+adama007@users.noreply.github.com>
2023-04-09 15:43:05 +0100

    update deployment

--- WebApplication01/Program.cs
+++ WebApplication01/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! hello world");
+app.MapGet("/", () => "taw takhrali fih");
 
 app.Run();
